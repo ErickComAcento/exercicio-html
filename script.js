2c59069fefe3fb74054e0a1063294471ca218956
Erick <ericklopesalmeida8@gmail.com>
2023-10-18 21:53:40 -0300

    modo escuro pt2

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-let trocar = document.querySelector("body")
+let trocar = document.querySelector("#botao")
+let body = document.querySelector("body")
 let eu = document.querySelector(".eu")
 let h1escuro = document.querySelector(".tracking-in-expand")
 let formulas = document.querySelector(".formulario")
 trocar.addEventListener("click", () => {
-    trocar.classList.toggle("modoescuro");
+    body.classList.toggle("modoescuro");
     eu.classList.toggle("modoescuroEu");
     h1escuro.classList.toggle("margem");
     formulas.classList.toggle("rodape")
